224d9d8e97079aae0acfb07c35ffd65f9426ab08
Michael Zhan <michaelzhan2001@gmail.com>
2025-06-23 22:23:28 -0400

    set up backend

---
+++ backend/eslint.config.js
@@ -0,0 +1,38 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  globalIgnores([
+    "**/node_modules/**",
+    "**/build/**",
+    "**/dist/**",
+    "**/dist_vite/**",
+    "**/env/**",
+  ]),
+]);
