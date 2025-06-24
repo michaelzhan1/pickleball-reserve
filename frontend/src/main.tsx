a890efd92255feefe8bff96b5ff757776cbec0ea
Michael Zhan <michaelzhan2001@gmail.com>
2025-06-23 20:49:47 -0400

    configure repo

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import App from './App.tsx';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById('root');
+if (!root) throw new Error('Failed to load root element');
+
+createRoot(root).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
